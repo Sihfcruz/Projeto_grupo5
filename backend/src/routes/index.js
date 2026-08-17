bb30b6d259825d21546d6aaaced8298040e1c805
sihfcruz <simeia.ruz@aluno.senai.br>
2026-08-17 10:17:39 -0300

    index

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -28,7 +28,7 @@ router.get('/', (req, res) => {
     })
 })
 
-<<<<<<< HEAD
+
 const ProdutoRoutes = require('./ProdutoRoutes');
 const CargoRoutes = require('./CargoRoutes');
 const FuncionarioRoutes = require('./FuncionarioRoutes');
@@ -37,16 +37,10 @@ const EntradaRoutes = require('./EntradaRoutes');
 const SaidaRoutes = require('./SaidaRoutes');
 const DevolucaoRoutes = require('./DevolucaoRoutes')
 
-
-=======
-
-
-
->>>>>>> ab8520b2b3d35622a92cc9969db9ed2a73ee5f8a
 router.use('/produto', ProdutoRoutes);
-// router.use('/cargo', CargoRoutes);
+router.use('/cargo', CargoRoutes);
 router.use('/funcionarios', FuncionarioRoutes);
-// router.use('/estoque', EstoqueRoutes);
+router.use('/estoque', EstoqueRoutes);
 router.use('/entrada', EntradaRoutes);
 router.use('/saida', SaidaRoutes);
 router.use('/devolucao', DevolucaoRoutes);
