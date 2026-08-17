e5f39e5f4a2aaf9099524335abfc8e698574c626
sihfcruz <simeia.ruz@aluno.senai.br>
2026-08-17 08:33:58 -0300

    criação e ajustes das rotas no index

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -11,8 +11,28 @@ router.get('/', (req, res) => {
             '/funcionarios',
             '/entrada',
             '/saida',
+            '/produto',
+            '/cargo',
+            '/estoque',
+            'devolucao',
         ]
     })
 })
 
-module.exports = router
+const ProdutoRoutes = require('./ProdutoRoutes');
+const CargoRoutes = require('./CargoRoutes');
+const FuncionarioRoutes = require('./FuncionarioRoutes');
+const EstoqueRoutes = require('./EstoqueRoutes');
+const EntradaRoutes = require('./EntradaRoutes');
+const SaidaRoutes = require('./SaidaRoutes');
+const DevolucaoRoutes = require('./DevolucaoRoutes');
+
+router.use('/produto', ProdutoRoutes);
+router.use('/cargo', CargoRoutes);
+router.use('/funcionario', FuncionarioRoutes);
+router.use('/estoque', EstoqueRoutes);
+router.use('/entrada', EntradaRoutes);
+router.use('/saida', SaidaRoutes);
+router.use('/devolucao', DevolucaoRoutes);
+
+module.exports = router;
